8fb43d0f9a8e4efd293295bf5250ed70f8a837a2
drew gordon <drew.s.gordon@gmail.com>
2013-12-08 01:20:13 -0500

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+blacklight project app using the [solr_ead](https://github.com/awead/solr_ead, solr_ead gem). test. test.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EadDemo::Application.config.secret_token = 'f1a9baf7b5331d344b81ff0ac341f11c04a14c0c28566d4d2b115443da17085f7a07da8925cc29aa54b1f407af0e8fd26cf560919a6691ad1f318045872aedd5'
